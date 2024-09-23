eebb33342bedaf33e6d87d8433cf8ee6b3e1e416
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-09-24 03:41:39 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.mspj7nt.mongodb.net/?retryWrites=true&w=majority`;
+const connectionStr = `Your MongoDBURI`;
 mongoose.connect(connectionStr, {useNewUrlparser: true})
 .then(() => console.log('connected to mongodb'))
 .catch(err => console.log(err))  
